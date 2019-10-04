85a8ca7acdb6804e1465d5994068978ad274aa9e
Demyx <info@demyx.sh>
2019-10-03 20:02:46 -0700

    Change Travis commit message

--- travis.sh
+++ travis.sh
@@ -17,7 +17,7 @@ sed -i "s|code--server-.*.-informational|code--server-${DEMYX_CODE_VERSION}-info
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote set-url origin https://${DEMYX_GITHUB_TOKEN}@github.com/demyxco/"$DEMYX_REPOSITORY".git
-git add .; git commit -m "Weekly Travis Build $TRAVIS_BUILD_NUMBER"; git push origin HEAD:master
+git add .; git commit -m "Travis Build $TRAVIS_BUILD_NUMBER"; git push origin HEAD:master
 
 # Set the default path to README.md
 README_FILEPATH="./README.md"
