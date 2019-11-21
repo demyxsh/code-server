da9a8985a09876cdc3f9edbc3bbd7426e8235965
Demyx <info@demyx.sh>
2019-11-21 03:42:33 -0800

    Symlink docker creds to /root

--- tag-latest/demyx.sh
+++ tag-latest/demyx.sh
@@ -2,12 +2,17 @@
 # Demyx
 # https://demyx.sh
 
+# Execute command before starting up code-server
+"$@"
+
+# Symlink docker credentials to root user if ~/.docker exists
+if [[ -d /home/demyx/.docker ]]; then
+    sudo ln -s /home/demyx/.docker /root/.docker
+fi
+
 # Set default workdir
 if [[ -z "$CODER_WORKDIR" ]]; then
     CODER_WORKDIR=/home/demyx
 fi
-
-# Execute command before starting up code-server
-"$@"
 
 code-server $(echo "$CODER_WORKDIR") --user-data-dir=/home/demyx/.code/data --extensions-dir=/home/demyx/.code/extensions --disable-telemetry
