7b6521403ee189ff5bc894921109c7f5aebd7154
Demyx <info@demyx.sh>
2019-12-11 20:35:56 -0800

    Misc updates

--- tag-latest-alpine/demyx.sh
+++ tag-latest-alpine/demyx.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Demyx
 # https://demyx.sh
-
-# Execute command before starting up code-server
-"$@"
+set -euo pipefail
 
 # Symlink docker credentials to root user if ~/.docker exists
 if [[ -d /home/demyx/.docker ]]; then
-    sudo ln -s /home/demyx/.docker /root/.docker
+    sudo ln -sf /home/demyx/.docker /root/.docker
 fi
 
-# Set default workdir
-if [[ -z "$CODER_WORKDIR" ]]; then
-    CODER_WORKDIR=/home/demyx
+# first arg is `-f` or `--some-option`
+if [[ "${1#-}" != "$1" ]]; then
+	set -- php-fpm "$@"
 fi
 
-code-server $(echo "$CODER_WORKDIR") --user-data-dir=/home/demyx/.code/data --extensions-dir=/home/demyx/.code/extensions --disable-telemetry
+exec "$@"
--- tag-latest/demyx.sh
+++ tag-latest/demyx.sh
@@ -3,15 +3,14 @@
 # https://demyx.sh
 set -euo pipefail
 
-# Execute command before starting up code-server
-"$@"
-
 # Symlink docker credentials to root user if ~/.docker exists
 if [[ -d /home/demyx/.docker ]]; then
-    sudo ln -s /home/demyx/.docker /root/.docker
+    sudo ln -sf /home/demyx/.docker /root/.docker
 fi
 
-# Set default workdir
-CODER_WORKDIR="${CODER_WORKDIR:-/home/demyx}"
+# first arg is `-f` or `--some-option`
+if [[ "${1#-}" != "$1" ]]; then
+	set -- php-fpm "$@"
+fi
 
-code-server $(echo "$CODER_WORKDIR") --user-data-dir=/home/demyx/.code/data --extensions-dir=/home/demyx/.code/extensions --disable-telemetry
+exec "$@"
