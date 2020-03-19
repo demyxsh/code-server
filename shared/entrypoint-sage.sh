119c9b446a2fd6e95feebb86c521b62855423daa
Demyx <info@demyx.sh>
2020-03-18 23:58:42 -0700

    Add scripts for sage variant

---
+++ shared/entrypoint-sage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/dumb-init /bin/bash
+# Demyx
+# https://demyx.sh
+set -euo pipefail
+
+# Compatibility with old paths
+[[ -d /var/www/html/wp-admin ]] && CODE_SERVER_ROOT=/var/www/html
+
+# Generate config
+demyx-config
+
+# Auto install WordPress if it's not installed already
+demyx-install
+
+# Install demyx helper plugin
+cp "$CODE_SERVER_CONFIG"/bs.php "$CODE_SERVER_ROOT"/web/app/mu-plugins
+
+# Configure xdebug
+if [[ ! -d "$CODE_SERVER_ROOT"/.vscode ]]; then
+    install -d -m 0755 -o demyx -g demyx "$CODE_SERVER_ROOT"/.vscode
+    mv "$CODE_SERVER_CONFIG"/launch.json "$CODE_SERVER_ROOT"/.vscode
+fi
+
+# Set Bedrock to debug mode
+sed -i "s|WP_ENV=.*|WP_ENV=development|g" "$CODE_SERVER_ROOT"/.env
+
+# Start php-fpm in the background
+php-fpm -D
+
+code-server ${CODE_SERVER_ROOT} --host=0.0.0.0 --user-data-dir=/home/demyx/.code/data --extensions-dir=/home/demyx/.code/extensions --disable-telemetry --disable-updates --port=8080
